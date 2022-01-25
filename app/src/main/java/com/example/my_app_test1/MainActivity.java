919f153231c5e661a7d9aeedeb42036f9b27aafb
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-25 23:34:06 +0900

    OK なんか画面遷移のときに別の画面になってるのがめっちゃ分かる

--- app/src/main/java/com/example/my_app_test1/MainActivity.java
+++ app/src/main/java/com/example/my_app_test1/MainActivity.java
@@ -11,12 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_title);
-
-        Button sendButton = findViewById(R.id.start_button);
-        sendButton.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplication(), TitleActivity.class);
-            startActivity(intent);
-        });
+        setContentView(R.layout.activity_home);
     }
 }
--- app/src/main/java/com/example/my_app_test1/TitleActivity.java
+++ app/src/main/java/com/example/my_app_test1/TitleActivity.java
@@ -3,12 +3,20 @@ package com.example.my_app_test1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class TitleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_title);
+
+        Button start_Button = findViewById(R.id.start_button);
+        start_Button.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
