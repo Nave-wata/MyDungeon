15c87b09256033c716094d9fb2ee74c2367ac0f0
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-25 23:16:38 +0900

    title用のscript追加

--- app/src/main/java/com/example/my_app_test1/TitleActivity.java
+++ app/src/main/java/com/example/my_app_test1/TitleActivity.java
@@ -1,21 +1,14 @@
 package com.example.my_app_test1;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class TitleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_title);
-    }
-
-    public void start_button(View view) {
-
     }
 }
