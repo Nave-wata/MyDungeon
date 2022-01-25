e62f2e2ea30dee6d4338b3b22b3a3b059716a84a
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-26 01:18:37 +0900

    やっとできたぞ！

--- app/src/main/java/com/example/my_app_test1/MainActivity.java
+++ app/src/main/java/com/example/my_app_test1/MainActivity.java
@@ -1,28 +1,40 @@
 package com.example.my_app_test1;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setScreenTitle();
-    }
-
-    private void setScreenTitle() {
-        setContentView(R.layout.activity_title);
-
-        Button title_Button = findViewById(R.id.start_button);
-        title_Button.setOnClickListener(v -> setScreenMain());
-    }
-
-    private void setScreenMain() {
         setContentView(R.layout.activity_home);
+
+        ImageView imageView = (ImageView)findViewById(R.id.image_view);
+        Bitmap image = getBitmapFromAsset("title.png");
+        imageView.setImageBitmap(image);
+    }
+
+
+    private Bitmap getBitmapFromAsset(String strName)
+    {
+        AssetManager assetManager = getAssets();
+        InputStream istr = null;
+        try {
+            istr = assetManager.open(strName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Bitmap bitmap = BitmapFactory.decodeStream(istr);
+        return bitmap;
     }
 }
