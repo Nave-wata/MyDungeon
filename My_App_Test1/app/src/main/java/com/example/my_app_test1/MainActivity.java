d49475b341be516f4f85274b0d03fc7123e5e0ba
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-26 16:00:08 +0900

    多分他のクラスでやるのは無理っぽい

--- My_App_Test1/app/src/main/java/com/example/my_app_test1/MainActivity.java
+++ My_App_Test1/app/src/main/java/com/example/my_app_test1/MainActivity.java
@@ -17,18 +17,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_title);
 
         // タイトル
         setScreenTitle();
     }
 
+
+    private void getImageViewBitmapFromAsset(String strName, String strID) {
+        int ID = getResources().getIdentifier(strID, "id", getPackageName());
+        ImageView imageView = (ImageView)findViewById(ID);
+        AssetManager assetManager = getAssets();
+        InputStream istr = null;
+
+        try {
+            istr = assetManager.open(strName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Bitmap bitmap = BitmapFactory.decodeStream(istr);
+        imageView.setImageBitmap(bitmap);
+    }
+
     private void setScreenTitle() {
         setContentView(R.layout.activity_title);
+        Hoge hoge = new Hoge();
 
         // 画像
-        GetBitmapFromAsset getBitmap = new GetBitmapFromAsset();
-        getBitmap.getImageViewBitmapFromAsset("title/title.png", "image_view");
+        hoge.getImageViewBitmapFromAsset("title/title.png", "image_view");
 
         // スタートボタン
         Button title_Button = findViewById(R.id.start_button);
@@ -37,5 +53,23 @@ public class MainActivity extends AppCompatActivity {
 
     private void setScreenMain() {
         setContentView(R.layout.activity_home);
+    }
+}
+
+class Hoge extends AppCompatActivity {
+    public void getImageViewBitmapFromAsset(String strName, String strID) {
+        int ID = getResources().getIdentifier(strID, "id", getPackageName());
+        ImageView imageView = (ImageView)findViewById(ID);
+        AssetManager assetManager = getAssets();
+        InputStream istr = null;
+
+        try {
+            istr = assetManager.open(strName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Bitmap bitmap = BitmapFactory.decodeStream(istr);
+        imageView.setImageBitmap(bitmap);
     }
 }
