d2db601b512b4e1d2e808dbd0ec7ee8289bae789
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-30 15:00:03 +0900

    Create Entity

---
+++ DB_Test3/app/src/main/java/com/example/db_test3/Texts.java
@@ -0,0 +1,14 @@
+package com.example.db_test3;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "TextData")
+public class Texts {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "Text")
+    public String Text;
+}
