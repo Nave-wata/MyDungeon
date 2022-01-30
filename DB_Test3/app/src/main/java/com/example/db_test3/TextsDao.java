cc6f125ba2dbb23d52dd5ef98ce9517b479d0422
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-30 15:14:43 +0900

    Create Dao

--- DB_Test3/app/src/main/java/com/example/db_test3/TextsDao.java
+++ DB_Test3/app/src/main/java/com/example/db_test3/TextsDao.java
@@ -1,2 +1,18 @@
-package com.example.db_test3;public class TextsDao {
+package com.example.db_test3;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Update;
+import androidx.room.Delete;
+
+@Dao
+public interface TextsDao {
+    @Insert
+    void insert(Texts Text);
+
+    @Update
+    void update(Texts Text);
+
+    @Delete
+    void delete(Texts Text);
 }
