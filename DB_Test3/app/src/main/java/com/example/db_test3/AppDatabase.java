d451880c85219de83f9fc73f64e9c840aaf4d46e
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-30 15:26:25 +0900

    Rename

--- DB_Test3/app/src/main/java/com/example/db_test3/AppDatabase.java
+++ DB_Test3/app/src/main/java/com/example/db_test3/AppDatabase.java
@@ -4,6 +4,6 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Texts.class}, version = 1)
-public abstract class TextsDatabase extends RoomDatabase {
+public abstract class AppDatabase extends RoomDatabase {
     public abstract TextsDao textsDao();
 }
