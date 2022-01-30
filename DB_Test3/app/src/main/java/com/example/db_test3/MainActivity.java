e31ea6f782636a53163645e1c23f95b3d919a95b
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-30 18:48:31 +0900

    AsyncTaskを使ってみる

--- DB_Test3/app/src/main/java/com/example/db_test3/MainActivity.java
+++ DB_Test3/app/src/main/java/com/example/db_test3/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.db_test3;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import java.util.List;
 
@@ -12,21 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        AppDatabase db = AppDatabaseSingleton.getInstance(getApplicationContext());
-
-        test(db);
-    }
-
-    private void test(AppDatabase db) {
-        TextsDao textsDao = db.textsDao();
-
-        textsDao.insert(new Texts("Hello"));
-        //Log.v("testsDao.insert", "OK!");
-
-        //List<Texts> texts = textsDao.getAll();
-        //for(Texts text: texts) {
-        //    Log.v("textsDao.getAll()", "" + text);
-        //}
     }
 }
