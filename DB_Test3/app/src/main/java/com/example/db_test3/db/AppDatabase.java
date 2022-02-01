4b21b4be4b54f134a8e198efcc9a407122c8ca6c
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-01 19:40:49 +0900

    Daoをabstractでやらないとあかん

--- DB_Test3/app/src/main/java/com/example/db_test3/db/AppDatabase.java
+++ DB_Test3/app/src/main/java/com/example/db_test3/db/AppDatabase.java
@@ -6,9 +6,10 @@ import androidx.room.RoomDatabase;
 import com.example.db_test3.db.tests.Texts;
 import com.example.db_test3.db.tests.TextsDao;
 import com.example.db_test3.db.usersinfo.UsersInfo;
+import com.example.db_test3.db.usersinfo.UsersInfoDao;
 
 @Database(entities = {Texts.class, UsersInfo.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract TextsDao textsDao();
-    public abstract UsersInfo usersInfo();
+    public abstract UsersInfoDao usersInfoDao();
 }
