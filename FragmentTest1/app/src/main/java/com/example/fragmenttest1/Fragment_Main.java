72abfdfce44f62ec959a629192098167603d6dfd
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-14 16:47:32 +0900

    Fragment内でもsetText可能らしい

--- FragmentTest1/app/src/main/java/com/example/fragmenttest1/Fragment_Main.java
+++ FragmentTest1/app/src/main/java/com/example/fragmenttest1/Fragment_Main.java
@@ -6,12 +6,12 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Fragment_Main extends Fragment {
-    static {
-        System.loadLibrary("fragmenttest1");
-    }
+    static { System.loadLibrary("fragmenttest1"); }
     public native String stringFromJNI();
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container,
@@ -19,5 +19,13 @@ public class Fragment_Main extends Fragment {
 
         return inflater.inflate(R.layout.fragment_main,
                 container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView textView = (TextView) view.findViewById(R.id.HelloNative);
+        textView.setText("HelloNative");
     }
 }
