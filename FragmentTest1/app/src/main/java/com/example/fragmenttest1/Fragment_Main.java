5253c8567f41b900e7b3139b4b699f9cc7f84d3c
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-15 12:44:13 +0900

    呼び出しOK!

--- FragmentTest1/app/src/main/java/com/example/fragmenttest1/Fragment_Main.java
+++ FragmentTest1/app/src/main/java/com/example/fragmenttest1/Fragment_Main.java
@@ -11,11 +11,13 @@ import android.widget.SeekBar;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-public class Fragment_Main extends Fragment implements SeekBar.OnSeekBarChangeListener {
+public class Fragment_Main extends Fragment {
     static { System.loadLibrary("fragmenttest1"); }
     public native String stringFromJNI();
 
-    private Button bt;
+    Button bt;
+    SeekBar sb1;
+    SeekBar sb2;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -27,21 +29,30 @@ public class Fragment_Main extends Fragment implements SeekBar.OnSeekBarChangeLi
         super.onViewCreated(view, savedInstanceState);
 
         bt = view.findViewById(R.id.button);
+        sb1 = view.findViewById(R.id.seekBar1);
+        sb2 = view.findViewById(R.id.seekBar2);
+
+        onSeekBarChangeListener seekBars = new onSeekBarChangeListener();
+
+        sb1.setOnSeekBarChangeListener(seekBars);
+        sb2.setOnSeekBarChangeListener(seekBars);
     }
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-        String str = String.valueOf(i);
-        bt.setText(str);
-    }
+    private class onSeekBarChangeListener implements SeekBar.OnSeekBarChangeListener {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            String str = String.valueOf(i);
+            bt.setText(str);
+        }
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
 
-    }
+        }
 
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
 
+        }
     }
 }
