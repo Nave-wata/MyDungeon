2cc2d347a3f105ba6760efe1463605e1cf66aab9
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-15 12:17:07 +0900

    Fragmentもandroidx

--- FragmentTest1/app/src/main/java/com/example/fragmenttest1/Fragment_Main.java
+++ FragmentTest1/app/src/main/java/com/example/fragmenttest1/Fragment_Main.java
@@ -1,12 +1,12 @@
 package com.example.fragmenttest1;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 public class Fragment_Main extends Fragment {
     static { System.loadLibrary("fragmenttest1"); }
@@ -15,5 +15,11 @@ public class Fragment_Main extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
