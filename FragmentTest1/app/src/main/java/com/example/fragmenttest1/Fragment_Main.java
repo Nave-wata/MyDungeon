b298877255e1291b2ad505e397fedbf2eb880183
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-15 12:37:07 +0900

    seekbarの変更で呼び出されてない？

--- FragmentTest1/app/src/main/java/com/example/fragmenttest1/Fragment_Main.java
+++ FragmentTest1/app/src/main/java/com/example/fragmenttest1/Fragment_Main.java
@@ -1,16 +1,21 @@
 package com.example.fragmenttest1;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-public class Fragment_Main extends Fragment {
+public class Fragment_Main extends Fragment implements SeekBar.OnSeekBarChangeListener {
     static { System.loadLibrary("fragmenttest1"); }
     public native String stringFromJNI();
+
+    private Button bt;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -20,6 +25,23 @@ public class Fragment_Main extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        bt = view.findViewById(R.id.button);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        String str = String.valueOf(i);
+        bt.setText(str);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
 }
