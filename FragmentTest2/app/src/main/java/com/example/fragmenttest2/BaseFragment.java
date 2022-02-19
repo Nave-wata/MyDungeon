58c37791ff9482b6ca99a7bd35c91a76b700a24a
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-20 01:24:14 +0900

    ダメか

--- FragmentTest2/app/src/main/java/com/example/fragmenttest2/BaseFragment.java
+++ FragmentTest2/app/src/main/java/com/example/fragmenttest2/BaseFragment.java
@@ -1,8 +1,12 @@
 package com.example.fragmenttest2;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,13 +17,19 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.fragmenttest2.asynchronous.AsyncRunnable;
-import com.example.fragmenttest2.asynchronous.HttpRequestor;
 import com.example.fragmenttest2.dungeon.DungeonFragment;
 import com.example.fragmenttest2.home.MainFragment;
 import com.example.fragmenttest2.monster.MonsterFragment;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 
 public class BaseFragment extends Fragment {
@@ -73,7 +83,7 @@ public class BaseFragment extends Fragment {
             switch (v.getId()) {
                 case R.id.home_button:
                     if (homeFlag) {
-                        new HttpRequestor(
+                        new AsyncRunnable(
                                 getContext(),
                                 "https://google.com",
                                 "読み込み中",
@@ -112,6 +122,114 @@ public class BaseFragment extends Fragment {
                 default:
                     break;
             }
+        }
+    }
+
+    public class AsyncRunnable implements Runnable {
+        private Context context = null;
+        private String url = null;
+        private String message = null;
+        private Consumer<byte[]> callback = null;
+        private Consumer<Exception> errorCallback = null;
+        private Exception exception = null;
+        private ProgressDialog progressDialog = null;
+        private byte[] response;
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        /**
+         　 * コンストラクタ
+         　 * @param context コンテキスト
+         　 * @param url 通信先URL
+         　 * @param message 処理中メッセージ
+         　 * @param callback 正常時のコールバック関数(Consumer<byte[]>)
+         　 * @param errorCallback エラー時のコールバック関数(Consumer<Exception>)
+         　 */
+        public AsyncRunnable(Context context,
+                             String url,
+                             String message,
+                             Consumer<byte[]> callback,
+                             Consumer<Exception> errorCallback) {
+            this.context = context;
+            this.url = url;
+            this.message = message;
+            this.callback = callback;
+            this.errorCallback = errorCallback;
+        }
+
+
+        @Override
+        public void run() {
+            onPreExecute();
+            response = doInBackground();
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    onPostExecute(response);
+                }
+            });
+        }
+
+        public void execute() {
+            ExecutorService executorService  = Executors.newSingleThreadExecutor();
+            executorService.submit(new com.example.fragmenttest2.asynchronous.AsyncRunnable(context, url, message, callback, errorCallback));
+        }
+
+        void onPreExecute() {
+            // 砂時計表示
+            this.progressDialog = new ProgressDialog(context);
+            this.progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            this.progressDialog.setMessage(this.message);
+            progressDialog.setCancelable(false);
+            this.progressDialog.show();
+        }
+
+        byte[] doInBackground() {
+            HttpURLConnection con = null;
+            try {
+                // HTTPリクエスト
+                final URL url = new URL(this.url);
+                con = (HttpURLConnection) url.openConnection();
+                con.connect();
+
+                final int status = con.getResponseCode();
+                if (status == HttpURLConnection.HTTP_OK) {
+                    final InputStream in = con.getInputStream();
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    byte [] buffer = new byte[1024];
+                    while(true) {
+                        int len = in.read(buffer);
+                        if(len < 0) {
+                            break;
+                        }
+                        out.write(buffer, 0, len);
+                    }
+                    in.close();
+                    out.close();
+                    return out.toByteArray();
+                } else {
+                    throw new IOException("HTTP status:" + status);
+                }
+            } catch (Exception e) {
+                Log.e("ERROR", e.toString(), e);
+                this.exception = e;
+                return null;
+            } finally {
+                if (con != null) {
+                    con.disconnect();
+                }
+            }
+        }
+
+        @SuppressLint("NewApi")
+        void onPostExecute(byte[] response) {
+            // コールバック処理実行
+            if (this.exception == null) {
+                callback.accept(response);
+            } else {
+                errorCallback.accept(this.exception);
+            }
+            // 砂時計解除
+            this.progressDialog.dismiss();
         }
     }
 }
