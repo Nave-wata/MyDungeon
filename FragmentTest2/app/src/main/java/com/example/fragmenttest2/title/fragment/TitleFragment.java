134cdf9694eed98552750d69f65b5913e50cef19
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-20 23:28:43 +0900

    接続＆完了まで待機OK!!

--- FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/fragment/TitleFragment.java
+++ FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/fragment/TitleFragment.java
@@ -1,7 +1,9 @@
 package com.example.fragmenttest2.title.fragment;
 
 import android.content.res.AssetManager;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +21,13 @@ import com.example.fragmenttest2.title.activity.TitleActivity;
 import java.util.Objects;
 
 public class TitleFragment extends Fragment {
+
+    public String[] URLs = new String[] {"https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060",
+            "https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060",
+            "https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060"};
+    public String[] str = new String[URLs.length];
+    public ImageButton imageButton;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_title, container, false);
@@ -31,12 +40,12 @@ public class TitleFragment extends Fragment {
         AssetManager assetManager = Objects.requireNonNull(getActivity()).getAssets();
         SetImage setImage = new SetImage(assetManager);
         ImageView imageView = view.findViewById(R.id.image_view1);
-        ImageButton imageButton = view.findViewById(R.id.image_button1);
+        imageButton = view.findViewById(R.id.image_button1);
 
         setImage.setImageViewBitmapFromAsset(imageView, "title/title.png");
         setImage.setImageButtonBitmapFromAsset(imageButton, "title/start.png");
 
-        imageButton.setOnClickListener(new onClickListener());
+        tmp();
     }
 
     public class onClickListener implements ImageButton.OnClickListener {
@@ -50,5 +59,46 @@ public class TitleFragment extends Fragment {
                 activity.ChangeActivity();
             }
         }
+    }
+
+    private void tmp() {
+        new AsyncRunnable(
+                URLs[0],
+                b->{
+                    str[0] = new String(b);
+                    Log.v("str1 = ", str[0]);
+                    imageButton.setOnClickListener(new onClickListener());
+                },
+                e->{
+                    str[0] = "Not found";
+                    imageButton.setOnClickListener(new onClickListener());
+                }
+        ).execute();
+
+        new AsyncRunnable(
+                URLs[1],
+                b->{
+                    str[1] = new String(b);
+                    Log.v("str2 = ", str[1]);
+                    imageButton.setOnClickListener(new onClickListener());
+                },
+                e->{
+                    str[1] = "Not found";
+                    imageButton.setOnClickListener(new onClickListener());
+                }
+        ).execute();
+
+        new AsyncRunnable(
+                URLs[2],
+                b->{
+                    str[2] = new String(b);
+                    Log.v("str3 = ", str[2]);
+                    imageButton.setOnClickListener(new onClickListener());
+                },
+                e->{
+                    str[2] = "Not found";
+                    imageButton.setOnClickListener(new onClickListener());
+                }
+        ).execute();
     }
 }
