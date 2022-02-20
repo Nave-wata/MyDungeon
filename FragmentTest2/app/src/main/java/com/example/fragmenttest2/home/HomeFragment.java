40528ea8ec4ecb48d3e9244e9d820b5934f0fdd6
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-21 00:09:08 +0900

    Fragment遷移時にあたい渡しOK!!

--- FragmentTest2/app/src/main/java/com/example/fragmenttest2/home/HomeFragment.java
+++ FragmentTest2/app/src/main/java/com/example/fragmenttest2/home/HomeFragment.java
@@ -12,6 +12,14 @@ import com.example.fragmenttest2.R;
 
 public class HomeFragment extends Fragment {
     TextView mainText;
+    String str;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        str = args.getString("Message");
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,10 +30,11 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mainText = view.findViewById(R.id.Home_text);
+        setHomeText(str);
     }
 
-    public void setHomeText(String str) {
-        mainText.setText(str);
+    public void setHomeText(String strtest) {
+        mainText.setText(strtest);
     }
 
 
