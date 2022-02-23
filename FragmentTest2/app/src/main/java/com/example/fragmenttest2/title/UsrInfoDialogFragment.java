4b6479852484a8319d24144da05deb75d5d7060f
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-23 15:14:14 +0900

    Default layout 今のところxmlファイルは使ってない

--- FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/UsrInfoDialogFragment.java
+++ FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/UsrInfoDialogFragment.java
@@ -1,6 +1,33 @@
 package com.example.fragmenttest2.title;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import com.example.fragmenttest2.BuildConfig;
+
 public class UsrInfoDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(@NonNull Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle("Title")
+                .setMessage("Message")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.v("Click", "DialogFragmentButton");
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .setNeutralButton("After", null);
+
+        return builder.create();
+    }
 }
