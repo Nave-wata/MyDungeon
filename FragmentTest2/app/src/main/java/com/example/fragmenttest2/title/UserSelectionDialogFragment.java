df04de948dc47ae10183d24808869869654c8fec
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-23 20:01:09 +0900

    レイアウトを表示させた

--- FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/UserSelectionDialogFragment.java
+++ FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/UserSelectionDialogFragment.java
@@ -5,28 +5,22 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.fragmenttest2.BuildConfig;
+import com.example.fragmenttest2.R;
 
 public class UserSelectionDialogFragment extends DialogFragment {
 
     @Override
-    public Dialog onCreateDialog(@NonNull Bundle savedInstanceState) {
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
 
-        builder.setTitle("Title")
-                .setMessage("Message")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.v("Click", "DialogFragmentButton");
-                    }
-                })
-                .setNegativeButton("Cancel", null)
-                .setNeutralButton("After", null);
+        builder.setView(inflater.inflate(R.layout.dialog_userselection, null));
 
         return builder.create();
     }
