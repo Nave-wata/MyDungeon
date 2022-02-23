a1ae5b09dfbaa44aaf8d5c199d164fb8bbe9aa38
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-23 21:45:59 +0900

    やり方分からんから`LinearLayout`使おう

--- FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/UserSelectionDialog.java
+++ FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/UserSelectionDialog.java
@@ -2,6 +2,7 @@ package com.example.fragmenttest2.title;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 
@@ -14,12 +15,22 @@ import com.example.fragmenttest2.R;
 public class UserSelectionDialog extends DialogFragment {
 
     @Override
-    public Dialog onCreateDialog(@NonNull Bundle savedInstanceState) {
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
 
-        builder.setView(inflater.inflate(R.layout.dialog_userselection, null));
+        builder.setView(inflater.inflate(R.layout.dialog_userselection, null))
+                .setPositiveButton("Test1", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
+                    }
+                })
+                .setNegativeButton("test2", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
         return builder.create();
     }
 }
