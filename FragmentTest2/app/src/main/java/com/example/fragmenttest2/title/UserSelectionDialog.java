70b7056fb1363eed95f830c8db9aa4f79f014328
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-23 23:50:59 +0900

    Textは変わらん

--- FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/UserSelectionDialog.java
+++ FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/UserSelectionDialog.java
@@ -3,34 +3,62 @@ package com.example.fragmenttest2.title;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import com.example.fragmenttest2.BaseFragment;
 import com.example.fragmenttest2.R;
+import com.example.fragmenttest2.SetImage;
+
+import java.util.Objects;
 
 
 public class UserSelectionDialog extends DialogFragment {
+    Button moveSelectUserPage_btn;
+    Button moveUserAddPage_btn;
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        moveSelectUserPage_btn = view.findViewById(R.id.moveSelectUserPage_button);
+        moveUserAddPage_btn =  view.findViewById(R.id.moveUserAddPage_button);
+
+        onClickListener bt = new onClickListener();
+
+        moveSelectUserPage_btn.setOnClickListener(bt);
+        moveUserAddPage_btn.setOnClickListener(bt);
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
 
-        builder.setView(inflater.inflate(R.layout.dialog_userselection, null))
-                .setPositiveButton("Test1", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+        builder.setView(inflater.inflate(R.layout.dialog_userselection, null));
 
-                    }
-                })
-                .setNegativeButton("test2", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-
-                    }
-                });
         return builder.create();
+    }
+
+    public class onClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.moveSelectUserPage_button:
+                    moveSelectUserPage_btn.setText("Push");
+                    break;
+                case R.id.moveUserAddPage_button:
+                    moveUserAddPage_btn.setText("Push");
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
