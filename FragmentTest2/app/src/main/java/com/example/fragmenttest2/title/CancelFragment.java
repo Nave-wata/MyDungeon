750442dcb9d9a5a31a483b17f84a87283f4af77f
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-24 15:42:19 +0900

    error ダイアログ2回目押すと強制終了する

---
+++ FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/CancelFragment.java
@@ -0,0 +1,46 @@
+package com.example.fragmenttest2.title;
+
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.fragmenttest2.BaseFragment;
+import com.example.fragmenttest2.R;
+import com.example.fragmenttest2.SetImage;
+
+import java.util.Objects;
+
+public class CancelFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cancel, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        AssetManager assetManager = Objects.requireNonNull(getActivity()).getAssets();
+        SetImage setImage = new SetImage(assetManager);
+
+        ImageButton btn = view.findViewById(R.id.cancel_button);
+        setImage.setImageButtonBitmapFromAsset(btn, "title/cancel.png");
+
+        btn.setOnClickListener(new onClickListener());
+    }
+
+    class onClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Log.v("Click", "Cancel_Button");
+        }
+    }
+}
