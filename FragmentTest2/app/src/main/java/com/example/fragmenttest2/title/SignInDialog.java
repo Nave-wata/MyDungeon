95eba44cd844e343602d6afe4966fd0de7a84f16
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-24 23:12:26 +0900

    EditTextの取得ができてない

--- FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/SignInDialog.java
+++ FragmentTest2/app/src/main/java/com/example/fragmenttest2/title/SignInDialog.java
@@ -3,8 +3,10 @@ package com.example.fragmenttest2.title;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,14 +15,19 @@ import androidx.fragment.app.DialogFragment;
 import com.example.fragmenttest2.R;
 
 public class SignInDialog extends DialogFragment {
-    Button btn;
-
     @Override
     public Dialog onCreateDialog(@NonNull Bundle savedInstanceState) {
         View view = requireActivity().getLayoutInflater().inflate(R.layout.dialog_signin, null);
 
-        btn = view.findViewById(R.id.SignIn_button);
-        btn.setOnClickListener(new onClickListener());
+        EditText ETName = view.findViewById(R.id.TitleETUserName);
+        EditText ETPass = view.findViewById(R.id.TitleETPassword);
+        String name = ETName.getText().toString();
+        String pass = ETPass.getText().toString();
+        Log.v("Name", name);
+        Log.v("Password", pass);
+
+        Button btn = view.findViewById(R.id.SignIn_button);
+        btn.setOnClickListener(new onClickListener(name, pass));
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()).setView(view);
         return builder.create();
@@ -28,8 +35,18 @@ public class SignInDialog extends DialogFragment {
 
 
     private class onClickListener implements View.OnClickListener {
+        String name;
+        String pass;
+
+        public onClickListener(String name, String pass) {
+            this.name = name;
+            this.pass = pass;
+        }
+
         @Override
         public void onClick(View v) {
+            Log.v("Name", name);
+            Log.v("Password", pass);
             dismiss();
         }
     }
