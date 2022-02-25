d226e69090e806a67d83d649b839f286d8f9acc6
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-25 13:16:03 +0900

    C++追加

--- FragmentTest2/app/src/main/cpp/native-lib.cpp
+++ FragmentTest2/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,7 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_fragmenttest2_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+extern "C" JNIEXPORT jstring  extern "C" JNICALL
+Java_com_example_fragmenttest2_title_SignInDialog_HASH(JNIEnv* env, jclass clazz, const std::string password) {
+    return
 }
