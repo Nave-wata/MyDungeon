78ec33d641bbf5a82c5e7f0b05cf41c759fc445d
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-25 16:16:13 +0900

    動いた

--- FragmentTest2/app/src/main/cpp/native-lib.cpp
+++ FragmentTest2/app/src/main/cpp/native-lib.cpp
@@ -8,16 +8,9 @@ extern "C" JNIEXPORT jstring
 Java_com_example_fragmenttest2_title_SignInDialog_HASH(JNIEnv* env, jclass clazz, jstring name, jstring password, int salt) {
     std::string Name = reinterpret_cast<std::basic_string<char> &&>(name);
     std::string Password = reinterpret_cast<std::basic_string<char> &&>(password);
-
-    unsigned int H[INIT_HASH_LENGTH];
     SHA256 sha256;
-    unsigned char** result = sha256.padding((char*)password);
 
-    sha256.compute(result, H);
-    result = sha256.padding((char*)H);
-    std::string output = sha256.hash_return(H);
-
-    sha256.free_block(result);
+    std::string output = sha256("Hello World");
 
     return env->NewStringUTF(output.c_str());
 }
