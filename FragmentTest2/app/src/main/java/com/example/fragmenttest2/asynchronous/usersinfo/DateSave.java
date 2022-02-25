3b4584d0de4f543ac975228eba59cc042497c3a8
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-26 00:07:10 +0900

    引数OK! ？

--- FragmentTest2/app/src/main/java/com/example/fragmenttest2/asynchronous/usersinfo/DateSave.java
+++ FragmentTest2/app/src/main/java/com/example/fragmenttest2/asynchronous/usersinfo/DateSave.java
@@ -3,11 +3,24 @@ package com.example.fragmenttest2.asynchronous.usersinfo;
 import android.os.Handler;
 import android.os.Looper;
 
+import com.example.fragmenttest2.asynchronous.AppDatabase;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class DateSave implements Runnable {
     Handler handler = new Handler(Looper.getMainLooper());
+    private AppDatabase db;
+    private String name;
+    private String salt;
+    private String hash;
+
+    public DateSave(AppDatabase db, String name, String salt, String hash) {
+        this.db = db;
+        this.name = name;
+        this.salt = salt;
+        this.hash = hash + "A";
+    }
 
     @Override
     public void run() {
@@ -18,7 +31,7 @@ public class DateSave implements Runnable {
     public void execute() {
         onPreExecute();
         ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.submit(new DateSave());
+        executorService.submit(new DateSave(db, name, salt, hash));
     }
 
     void onPreExecute() {}
