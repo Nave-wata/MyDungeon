68ad6cb6f0a1f2ed58ea402ef2ec73c7932fb7be
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-25 17:52:05 +0900

    変数名の重複やった

--- FragmentTest2/app/src/main/cpp/native-lib.cpp
+++ FragmentTest2/app/src/main/cpp/native-lib.cpp
@@ -7,7 +7,7 @@ std::string SHA256_SHA512(std::string basicString, std::string basicString1, int
 extern "C" JNIEXPORT jstring
 Java_com_example_fragmenttest2_title_SignInDialog_HASH(JNIEnv* env, jclass clazz, jstring password, jstring salt) {
     std::string Password = reinterpret_cast<std::basic_string<char> &&>(password);
-    std::string salt = reinterpret_cast<std::basic_string<char> &&>(salt);
+    std::string Salt = reinterpret_cast<std::basic_string<char> &&>(salt);
     SHA512 sha512;
 
     std::string output = sha512.hash(Password);
