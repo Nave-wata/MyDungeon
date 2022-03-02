cde7e34a80bc4ed6ea07e41a89f553f2344791a6
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-02 23:35:00 +0900

    検知できてない？

--- main/app/src/main/java/com/example/main/ApplicationManager.java
+++ main/app/src/main/java/com/example/main/ApplicationManager.java
@@ -1,18 +1,47 @@
 package com.example.main;
 
+import android.app.Activity;
 import android.app.Application;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 
-public class ApplicationManager extends Application {
+import androidx.annotation.NonNull;
+
+public class ApplicationManager extends Application implements Application.ActivityLifecycleCallbacks {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.v("MyApp", "Start");
     }
 
     @Override
-    public void onTerminate() {
-        super.onTerminate();
-        Log.v("MyApp", "End");
+    public void onActivityCreated(Activity activity, Bundle saveInstanceState) {}
+
+    @Override
+    public void onActivityStarted(Activity activity) {}
+
+    @Override
+    public void onActivityResumed(Activity activity) {}
+
+    @Override
+    public void onActivityPaused(@NonNull Activity activity) {}
+
+    @Override
+    public void onActivityStopped(Activity activity) {}
+
+    @Override
+    public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {}
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {}
+
+    class DestroyingService extends Service {
+        @Override
+        public IBinder onBind(Intent intent) {
+            Log.v("MyApp", "End");
+            return null;
+        }
     }
 }
