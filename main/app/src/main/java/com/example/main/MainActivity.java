5046f98a51e2140db183fee6ec445b368c40d416
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-03 00:38:37 +0900

    Dateは止めてLocalDateTimeで

--- main/app/src/main/java/com/example/main/MainActivity.java
+++ main/app/src/main/java/com/example/main/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.main;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -10,17 +12,27 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.main.title.TitleActivity;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 
 public class MainActivity extends AppCompatActivity {
     private String UserName;
+    private String Prev_Time = "PrevTime";
+    private String DS_Time = "Time";
+    private SharedPreferences.Editor editor;
+    private String beforeTimeStr;
+    private String afterTimeStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences dataStore = getSharedPreferences(Prev_Time, MODE_PRIVATE);
+        editor = dataStore.edit();
+
+        beforeTimeStr = dataStore.getString(Prev_Time, null);
 
         Intent intent = this.getIntent();
         UserName = intent.getStringExtra(TitleActivity.EXTRA_DATA);
@@ -31,11 +43,15 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
+    @SuppressLint("NewApi")
     @Override
     public void onPause() {
         super.onPause();
 
-        Date date = Calendar.getInstance().getTime();
-        Log.v("My Time", date.toString());
+        afterTimeStr = LocalDateTime.now().toString();
+        LocalDateTime beforeTime = LocalDateTime.parse(beforeTimeStr);
+        LocalDateTime afterTime = LocalDateTime.parse(afterTimeStr);
+
+        long Day = ChronoUnit.SECONDS.between(beforeTime, afterTime);
     }
 }
