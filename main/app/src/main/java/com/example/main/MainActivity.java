bff7e8e705813cbcd62ae2817ab660a4acfbdaa1
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-03 01:14:32 +0900

    強制終了

--- main/app/src/main/java/com/example/main/MainActivity.java
+++ main/app/src/main/java/com/example/main/MainActivity.java
@@ -21,21 +21,11 @@ import java.time.temporal.ChronoUnit;
 
 public class MainActivity extends AppCompatActivity {
     private String UserName;
-    private String Prev_Time = "PrevTime";
-    private String DS_Time = "Time";
-    private SharedPreferences.Editor editor;
-    private String beforeTimeStr;
-    private String afterTimeStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SharedPreferences dataStore = getSharedPreferences(Prev_Time, MODE_PRIVATE);
-        editor = dataStore.edit();
-
-        beforeTimeStr = dataStore.getString(Prev_Time, null);
 
         Intent intent = this.getIntent();
         UserName = intent.getStringExtra(TitleActivity.EXTRA_DATA);
@@ -51,15 +41,30 @@ public class MainActivity extends AppCompatActivity {
     public void onPause() {
         super.onPause();
 
-        int beforeYear = LocalDateTime.now().getYear();
-        int beforeMonth = LocalDateTime.now().getMonthValue();
-        int beforeDay = LocalDateTime.now().getDayOfMonth();
-        int beforeHour = LocalDateTime.now().getHour();
-        int beforeMinute = LocalDateTime.now().getMinute();
-        Log.v("My Time", "Year " + beforeYear + ", Month " + beforeMonth + ", Day " + beforeDay + ", Hour" + beforeHour + ", Minute" + beforeMinute);
+        SharedPreferences dataStore = getSharedPreferences("Prev_TIme", MODE_PRIVATE);
+        SharedPreferences.Editor editor = dataStore.edit();
 
-        LocalDateTime beginSummerVacation = LocalDateTime.of(2021, 7, 20, 11, 00);// 夏休みは終業式が終わったタイミングから
-        LocalDateTime endSummerVacation = LocalDateTime.of(2021, 9, 1, 8, 00);// 始業式が始まるタイミングまで
-        Duration summerVacationDuration = Duration.between(beginSummerVacation, endSummerVacation);// 期間分の時間を取得する
+        int nowYear = LocalDateTime.now().getYear();
+        int nowMonth = LocalDateTime.now().getMonthValue();
+        int nowDay = LocalDateTime.now().getDayOfMonth();
+        int nowHour = LocalDateTime.now().getHour();
+        int nowMinute = LocalDateTime.now().getMinute();
+        int beforeYear = dataStore.getInt("Year", 0);
+        int beforeMonth = dataStore.getInt("Month", 0);
+        int beforeDay = dataStore.getInt("Day", 0);
+        int beforeHour = dataStore.getInt("Hour", 0);
+        int beforeMinute = dataStore.getInt("Minute", 0);
+
+        editor.putInt("Year", nowYear);
+        editor.putInt("Month", nowMonth);
+        editor.putInt("Day", nowDay);
+        editor.putInt("Hour", nowHour);
+        editor.putInt("Minute", nowMinute);
+        editor.apply();
+
+        LocalDateTime beforeTime = LocalDateTime.of(beforeYear, beforeMonth, beforeDay, beforeHour, beforeMinute);
+        LocalDateTime nowTime = LocalDateTime.of(nowYear, nowMonth, nowDay, nowHour, nowMinute);
+        Duration duration = Duration.between(beforeTime, nowTime);// 期間分の時間を取得する
+        //duration.toMinutes();
     }
 }
