ec9091eacb27996153e48aba4343dd84a5c33f1d
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-03 01:35:50 +0900

    ユーザーごとに変わるからDBやん

--- main/app/src/main/java/com/example/main/MainActivity.java
+++ main/app/src/main/java/com/example/main/MainActivity.java
@@ -43,9 +43,6 @@ public class MainActivity extends AppCompatActivity {
     public void onPause() {
         super.onPause();
 
-        SharedPreferences dataStore = getSharedPreferences("Prev_TIme", MODE_PRIVATE);
-        SharedPreferences.Editor editor = dataStore.edit();
-
         LocalDateTime nowTime = LocalDateTime.now();
         int nowYear = nowTime.getYear();
         int nowMonth = nowTime.getMonthValue();
@@ -53,27 +50,5 @@ public class MainActivity extends AppCompatActivity {
         int nowHour = nowTime.getHour();
         int nowMinute = nowTime.getMinute();
         int nowSecond = nowTime.getSecond();
-
-        int beforeYear = dataStore.getInt("Year", 0);
-        int beforeMonth = dataStore.getInt("Month", 0);
-        int beforeDay = dataStore.getInt("Day", 0);
-        int beforeHour = dataStore.getInt("Hour", 0);
-        int beforeMinute = dataStore.getInt("Minute", 0);
-        int beforeSecond = dataStore.getInt("Second", 0);
-
-        editor.putInt("Year", nowYear);
-        editor.putInt("Month", nowMonth);
-        editor.putInt("Day", nowDay);
-        editor.putInt("Hour", nowHour);
-        editor.putInt("Minute", nowMinute);
-        editor.putInt("Second", nowSecond);
-        editor.apply();
-
-        LocalDateTime beforeTime = LocalDateTime.of(beforeYear, beforeMonth, beforeDay, beforeHour, beforeMinute);
-        LocalDateTime nowTimes = LocalDateTime.of(nowYear, nowMonth, nowDay, nowHour, nowMinute);
-        Duration duration = Duration.between(beforeTime, nowTimes);// 期間分の時間を取得する
-        long diff_min = duration.toMinutes();
-        long diff_sec = diff_min * 60 + (nowSecond - beforeSecond);
-        Log.v("diff_Minute", " = " + diff_sec);
     }
 }
