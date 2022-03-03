cdb2c7083bbbda4b6ad919833f998c4c30c2e757
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-03 14:41:14 +0900

    完全ランダムになってる？

--- main/app/src/main/cpp/native-lib.cpp
+++ main/app/src/main/cpp/native-lib.cpp
@@ -6,5 +6,10 @@ extern "C" JNIEXPORT jstring
 Java_com_example_fragmenttest2_title_SignUpDialog_HASH(JNIEnv* env, jclass clazz, jstring password, jstring salt) {
     std::string Password = reinterpret_cast<const char *>(password);
     std::string Salt = reinterpret_cast<const char *>(salt);
+    std::string result = "a";
     SHA512 sha512;
+
+    result = sha512.hash(result);
+
+    return (jstring) result.c_str();
 }
