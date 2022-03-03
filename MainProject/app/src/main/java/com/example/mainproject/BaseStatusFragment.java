eef017ca952096eb1187f0388d6d76304ce5b160
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-03 19:08:42 +0900

    Class作成

--- MainProject/app/src/main/java/com/example/mainproject/BaseStatusFragment.java
+++ MainProject/app/src/main/java/com/example/mainproject/BaseStatusFragment.java
@@ -1,4 +1,32 @@
 package com.example.mainproject;
 
-public class BaseStatusFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class BaseStatusFragment extends Fragment {
+    final String UserName;
+
+    public BaseStatusFragment(String UserName) {
+        this.UserName = UserName;
+    }
+
+    final String EXTRA_DATA = "com.example.mainproject";
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_basestatus, container, false);
+    }
+
+    @NonNull
+    public static BaseStatusFragment newInstance(String UserName){
+        BaseStatusFragment fragment = new BaseStatusFragment(UserName);
+        Bundle barg = new Bundle();
+        barg.putString(fragment.EXTRA_DATA, UserName);
+        fragment.setArguments(barg);
+        return fragment;
+    }
 }
