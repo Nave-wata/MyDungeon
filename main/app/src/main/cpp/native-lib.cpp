3c6a1af85c2742cf0961befa2301e44771343a31
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-03 14:34:05 +0900

    C++コード追加

--- main/app/src/main/cpp/native-lib.cpp
+++ main/app/src/main/cpp/native-lib.cpp
@@ -1,4 +1,10 @@
 #include <jni.h>
+#include <string>
+#include "SHA512.h"
 
 extern "C" JNIEXPORT jstring
-Java_com_example_fragmenttest2_title_SignUpDialog_HASH(JNIEnv* env, jclass clazz, jstring password1, jstring salt1) {}
+Java_com_example_fragmenttest2_title_SignUpDialog_HASH(JNIEnv* env, jclass clazz, jstring password, jstring salt) {
+    std::string Password = reinterpret_cast<const char *>(password);
+    std::string Salt = reinterpret_cast<const char *>(salt);
+    SHA512 sha512;
+}
