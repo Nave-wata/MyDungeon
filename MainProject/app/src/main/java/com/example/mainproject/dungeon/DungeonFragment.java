e416f9cb25479c49518d779f8072122d3334d414
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-03 16:32:21 +0900

    tmp

--- MainProject/app/src/main/java/com/example/mainproject/dungeon/DungeonFragment.java
+++ MainProject/app/src/main/java/com/example/mainproject/dungeon/DungeonFragment.java
@@ -1,13 +1,13 @@
 package com.example.mainproject.dungeon;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.example.mainproject.R;
 
