f34aadd12f5b89bc04c9b8ca2239a27bdbc8d927
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-08 00:03:17 +0900

    ImageView表示

--- MainProject/app/src/main/java/com/example/mainproject/dungeon/ConfirmCreateDungeonDialog.java
+++ MainProject/app/src/main/java/com/example/mainproject/dungeon/ConfirmCreateDungeonDialog.java
@@ -2,16 +2,34 @@ package com.example.mainproject.dungeon;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
+
+import com.example.mainproject.R;
+import com.example.mainproject.SetImage;
+
+import java.util.Objects;
 
 public class ConfirmCreateDungeonDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view = requireActivity().getLayoutInflater().inflate(R.layout.dialog_confirmcreatedungeon, null);
+        AssetManager assetManager = Objects.requireNonNull(getActivity()).getAssets();
+        SetImage setImage = new SetImage(assetManager);
+
+        ImageView ic_DP = view.findViewById(R.id.ConfirmCreateDungeonDialog_ic_DP);
+        ImageView ic_right = view.findViewById(R.id.ConfirmCreateDungeonDialog_right_ic);
+
+        setImage.setImageViewBitmapFromAsset(ic_DP, "base_menu/dungeonpower.png");
+        setImage.setImageViewBitmapFromAsset(ic_right, "dungeon/DP_change.png");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()).setView(view);
         builder.setPositiveButton("test", null);
         return builder.create();
     }
