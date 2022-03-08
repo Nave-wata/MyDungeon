5d8c0c20d58f426ec29c9a5de652dfc1b7249cb7
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-08 14:43:33 +0900

    分けた部分削除

--- MainProject/app/src/main/java/com/example/mainproject/asynchronous/usersinfo/UsersInfo.java
+++ MainProject/app/src/main/java/com/example/mainproject/asynchronous/usersinfo/UsersInfo.java
@@ -5,8 +5,6 @@ import androidx.room.Entity;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
-import com.example.mainproject.BaseStatusFragment;
-
 
 @Entity(tableName = "UsersInfo", indices = {@Index(value = "Name", unique = true)})
 public class UsersInfo {
@@ -22,49 +20,15 @@ public class UsersInfo {
     @ColumnInfo(name = "Hash")
     public String hash;
 
-    @ColumnInfo(name = "DP")
-    public String _DP;
-
-    @ColumnInfo(name = "Money")
-    public String _Money;
-
-    @ColumnInfo(name = "Year")
-    public int year;
-
-    @ColumnInfo(name = "Month")
-    public int month;
-
-    @ColumnInfo(name = "Day")
-    public int day;
-
-    @ColumnInfo(name = "Hour")
-    public int hour;
-
-    @ColumnInfo(name = "Minute")
-    public int minute;
-
-    @ColumnInfo(name = "Second")
-    public int second;
+    public UsersInfo(String name, String salt, String hash) {
+        this.name = name;
+        this.salt = salt;
+        this.hash = hash;
+    }
 
     public String getName() { return name; }
 
     public String getSalt() { return salt; }
 
     public String getHash() { return hash; }
-
-    public byte[] getDP() { return new BaseStatusFragment().CastByte(_DP); }
-
-    public byte[] getMoney() { return new BaseStatusFragment().CastByte(_Money); }
-
-    public int getYear() { return year; }
-
-    public int getMonth() { return month; }
-
-    public int getDay() { return day; }
-
-    public int getHour() { return hour; }
-
-    public int getMinute() { return minute; }
-
-    public int getSecond() { return second; }
 }
--- MainProject/app/src/main/java/com/example/mainproject/asynchronous/usersinfo/UsersInfoDao.java
+++ MainProject/app/src/main/java/com/example/mainproject/asynchronous/usersinfo/UsersInfoDao.java
@@ -1,6 +1,7 @@
 package com.example.mainproject.asynchronous.usersinfo;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
 import androidx.room.Query;
 
 import java.util.List;
@@ -8,25 +9,9 @@ import java.util.List;
 
 @Dao
 public interface UsersInfoDao {
-    @Query("INSERT INTO UsersInfo(Name, Salt, Hash, DP, Money, Year, Month, Day, Hour, Minute, Second) VALUES(:name, :salt, :hash, :dp, :money, :year, :month, :day, :hour, :minute, :second)")
-    void signUpTask(String name, String salt, String hash, String dp, String money, int year, int month, int day, int hour, int minute, int second);
-
-    @Query("UPDATE UsersInfo " +
-            "SET Year = :year, " +
-                "Month = :month, " +
-                "Day = :day, " +
-                "Hour = :hour, " +
-                "Minute = :minute, " +
-                "Second = :second " +
-            "WHERE Name = :name")
-    void updateTimeTask(String name, int year, int month, int day, int hour, int minute, int second);
-
-    @Query("UPDATE UsersInfo " +
-            "SET DP = :dp, " +
-                "Money = :money " +
-            "WHERE Name = :name")
-    void updatePossessionTask(String name, String dp, String money);
+    @Insert
+    void signUpTask(UsersInfo usersInfo);
 
     @Query("SELECT * FROM UsersInfo WHERE Name = :name")
-    List<UsersInfo> getLineTask(String name);
+    abstract List<UsersInfo> getUserInfoTask(String name);
 }
