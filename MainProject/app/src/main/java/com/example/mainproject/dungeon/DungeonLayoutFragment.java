0ae25d7e48bd4a9f61aca925341c371265b44780
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-10 21:42:59 +0900

    拡大縮小可能にする用のボタン追加

--- MainProject/app/src/main/java/com/example/mainproject/dungeon/DungeonLayoutFragment.java
+++ MainProject/app/src/main/java/com/example/mainproject/dungeon/DungeonLayoutFragment.java
@@ -7,11 +7,27 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.mainproject.R;
 
+import java.util.Objects;
+
 public class DungeonLayoutFragment extends Fragment {
     final String EXTRA_DATA = "com.example.mainproject.dungeon";
+    private String UserName;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        UserName = Objects.requireNonNull(args).getString(EXTRA_DATA);
+
+        FragmentTransaction fragmentTransaction = Objects.requireNonNull(getFragmentManager()).beginTransaction();
+        fragmentTransaction.replace(R.id.ScaleDungeonContainer, ScaleDungeonFragment.newInstance(UserName));
+        fragmentTransaction.commit();
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dungeonlayout, container, false);
