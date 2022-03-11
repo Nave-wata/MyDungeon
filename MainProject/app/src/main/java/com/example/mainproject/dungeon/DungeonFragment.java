4b962e76dd1fc2cc98fe07e8acb04e5dc0206797
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-11 14:15:41 +0900

    Dungeon内部のレイアウト変更用

--- MainProject/app/src/main/java/com/example/mainproject/dungeon/DungeonFragment.java
+++ MainProject/app/src/main/java/com/example/mainproject/dungeon/DungeonFragment.java
@@ -2,12 +2,9 @@ package com.example.mainproject.dungeon;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
@@ -21,10 +18,6 @@ import java.util.Objects;
 public class DungeonFragment extends Fragment {
     final String EXTRA_DATA = "com.example.mainproject.dungeon";
     private String UserName;
-    private androidx.constraintlayout.widget.ConstraintLayout topContainer;
-    private ViewTreeObserver.OnGlobalLayoutListener globalLayoutListener;
-    private int X, Y;
-    private int preDx, preDy;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,24 +33,6 @@ public class DungeonFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dungeon, container, false);
-        topContainer = view.findViewById(R.id.fragment_dungeon);
-        Log.i("", "MainActivityFragment#onCreateView() " +
-                "Width = " + String.valueOf(topContainer.getWidth()) + ", " +
-                "Height = " + String.valueOf(topContainer.getHeight()));
-
-        globalLayoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                Log.i("", "MainActivityFragment#onCreateView() " +
-                        "Width = " + String.valueOf(topContainer.getWidth()) + ", " +
-                        "Height = " + String.valueOf(topContainer.getHeight()));
-                X = topContainer.getWidth();
-                Y = topContainer.getHeight();
-                topContainer.getViewTreeObserver().removeOnGlobalLayoutListener(globalLayoutListener);
-            }
-        };
-        topContainer.getViewTreeObserver().addOnGlobalLayoutListener(globalLayoutListener);
-
         return view;
     }
 
@@ -67,34 +42,6 @@ public class DungeonFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Button createFlorButton = view.findViewById(R.id.createFlorButton);
-        createFlorButton.setOnTouchListener((view1, motionEvent) -> {
-            int newDx = (int) (motionEvent.getRawX() / 100) * 100;
-            int newDy = (int) (motionEvent.getRawY() / 100) * 100;
-
-            switch (motionEvent.getAction()) {
-                case MotionEvent.ACTION_MOVE:
-                    view1.performClick();
-                    int dx = createFlorButton.getLeft() + (newDx - preDx);
-                    int dy = createFlorButton.getTop() + (newDy - preDy);
-                    int imgW = dx + createFlorButton.getWidth();
-                    int imgH = dy + createFlorButton.getHeight();
-                    if (0 <= dx && dx < (X - 100) && 0 <= dy && dy < (Y - 100)) {
-                        createFlorButton.layout(dx, dy, imgW, imgH);
-                    }
-                    break;
-                case MotionEvent.ACTION_DOWN:
-                    Log.v("", ""+ createFlorButton.getLeft());
-                    Log.v("", "" + createFlorButton.getTop());
-                    // nothing to do
-                    break;
-                case MotionEvent.ACTION_UP:
-                    // nothing to do
-                    break;
-            }
-            preDx = newDx;
-            preDy = newDy;
-            return true;
-        });
     }
 
     @NonNull
--- MainProject/app/src/main/java/com/example/mainproject/dungeon/DungeonLayoutFragment.java
+++ MainProject/app/src/main/java/com/example/mainproject/dungeon/DungeonLayoutFragment.java
@@ -1,9 +1,13 @@
 package com.example.mainproject.dungeon;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,6 +19,10 @@ import java.util.Objects;
 public class DungeonLayoutFragment extends Fragment {
     final String EXTRA_DATA = "com.example.mainproject.dungeon";
     private String UserName;
+    private androidx.constraintlayout.widget.ConstraintLayout topContainer;
+    private ViewTreeObserver.OnGlobalLayoutListener globalLayoutListener;
+    private int X, Y;
+    private int preDx, preDy;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,7 +34,49 @@ public class DungeonLayoutFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dungeonlayout, container, false);
+
+        topContainer = view.findViewById(R.id.fragment_dungeonLayout);
+        globalLayoutListener = () -> {
+            X = topContainer.getWidth();
+            Y = topContainer.getHeight();
+            topContainer.getViewTreeObserver().removeOnGlobalLayoutListener(globalLayoutListener);
+        };
+        topContainer.getViewTreeObserver().addOnGlobalLayoutListener(globalLayoutListener);
         return view;
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView textView = view.findViewById(R.id.textView);
+        textView.setOnTouchListener((view1, motionEvent) -> {
+            int newDx = (int) (motionEvent.getRawX() / 100) * 100;
+            int newDy = (int) (motionEvent.getRawY() / 100) * 100;
+
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_MOVE:
+                    view1.performClick();
+                    int dx = textView.getLeft() + (newDx - preDx);
+                    int dy = textView.getTop() + (newDy - preDy);
+                    int imgW = dx + textView.getWidth();
+                    int imgH = dy + textView.getHeight();
+                    if (0 <= dx && dx < (X - 100) && 0 <= dy && dy < (Y - 100)) {
+                        textView.layout(dx, dy, imgW, imgH);
+                    }
+                    break;
+                case MotionEvent.ACTION_DOWN:
+                    // nothing to do
+                    break;
+                case MotionEvent.ACTION_UP:
+                    // nothing to do
+                    break;
+            }
+            preDx = newDx;
+            preDy = newDy;
+            return true;
+        });
     }
 
     @NonNull
