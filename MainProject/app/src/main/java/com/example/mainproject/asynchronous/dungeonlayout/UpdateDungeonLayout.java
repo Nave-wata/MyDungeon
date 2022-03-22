eb48fa217a835f5a3fec3d49c8e82b33c527e0ba
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-22 22:27:39 +0900

    保存ログはOK出た．しかし，保存後DungeonFragmentを呼び出すと強制終了

--- MainProject/app/src/main/java/com/example/mainproject/asynchronous/dungeonlayout/UpdateDungeonLayout.java
+++ MainProject/app/src/main/java/com/example/mainproject/asynchronous/dungeonlayout/UpdateDungeonLayout.java
@@ -20,6 +20,26 @@ public class UpdateDungeonLayout implements Runnable {
     final String name;
     final int[][] rows;
     private String[] data;
+    String row0;
+    String row1;
+    String row2;
+    String row3;
+    String row4;
+    String row5;
+    String row6;
+    String row7;
+    String row8;
+    String row9;
+    String row10 ;
+    String row11 ;
+    String row12 ;
+    String row13 ;
+    String row14 ;
+    String row15 ;
+    String row16 ;
+    String row17 ;
+    String row18 ;
+    String row19 ;
 
     public UpdateDungeonLayout(AppDatabase db,
                                String name,
@@ -63,53 +83,54 @@ public class UpdateDungeonLayout implements Runnable {
             }
             data[i] = sb.toString();
         }
+        row0 = data[0];
+        row1 = data[1];
+        row2 = data[2];
+        row3 = data[3];
+        row4 = data[4];
+        row5 = data[5];
+        row6 = data[6];
+        row7 = data[7];
+        row8 = data[8];
+        row9 = data[9];
+        row10 = data[10];
+        row11 = data[11];
+        row12 = data[12];
+        row13 = data[13];
+        row14 = data[14];
+        row15 = data[15];
+        row16 = data[16];
+        row17 = data[17];
+        row18 = data[18];
+        row19 = data[19];
+
+        Log.v("My", "" + row0);
+        Log.v("My", "" + row1);
+        Log.v("My", "" + row2);
+        Log.v("My", "" + row3);
+        Log.v("My", "" + row4);
+        Log.v("My", "" + row5);
+        Log.v("My", "" + row6);
+        Log.v("My", "" + row7);
+        Log.v("My", "" + row8);
+        Log.v("My", "" + row9);
+        Log.v("My", "" + row10);
+        Log.v("My", "" + row11);
+        Log.v("My", "" + row12);
+        Log.v("My", "" + row13);
+        Log.v("My", "" + row14);
+        Log.v("My", "" + row15);
+        Log.v("My", "" + row16);
+        Log.v("My", "" + row17);
+        Log.v("My", "" + row18);
+        Log.v("My", "" + row19);
     }
 
     void doInBackground() {
         DungeonLayoutDao dungeonLayoutDao = db.dungeonLayoutDao();
-        String row0 = data[0];
-        String row1 = data[1];
-        String row2 = data[2];
-        String row3 = data[3];
-        String row4 = data[4];
-        String row5 = data[5];
-        String row6 = data[6];
-        String row7 = data[7];
-        String row8 = data[8];
-        String row9 = data[9];
-        String row10 = data[10];
-        String row11 = data[11];
-        String row12 = data[12];
-        String row13 = data[13];
-        String row14 = data[14];
-        String row15 = data[15];
-        String row16 = data[16];
-        String row17 = data[17];
-        String row18 = data[18];
-        String row19 = data[19];
-
 
         try {
-            Log.v("My", "" + row0);
-            Log.v("My", "" + row1);
-            Log.v("My", "" + row2);
-            Log.v("My", "" + row3);
-            Log.v("My", "" + row4);
-            Log.v("My", "" + row5);
-            Log.v("My", "" + row6);
-            Log.v("My", "" + row7);
-            Log.v("My", "" + row8);
-            Log.v("My", "" + row9);
-            Log.v("My", "" + row10);
-            Log.v("My", "" + row11);
-            Log.v("My", "" + row12);
-            Log.v("My", "" + row13);
-            Log.v("My", "" + row14);
-            Log.v("My", "" + row15);
-            Log.v("My", "" + row16);
-            Log.v("My", "" + row17);
-            Log.v("My", "" + row18);
-            Log.v("My", "" + row19);
+
             dungeonLayoutDao.updateDungeonLayoutTask(
                     name,
                     row0,
